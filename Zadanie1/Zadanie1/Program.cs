a87fc63e329bdc328d768c60234b6144b9b99e0c
Lewiatann <maciek22130@gmail.com>
2021-02-18 01:24:48 +0100

    Add project files.

---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        private const char sym1 = '>';
+        private const char sym2 = '#';
+        private const int numberOfRepeat = 20;
+
+        static void Main(string[] args)
+        {
+            int numberOfLine = getNumberOfLines();
+
+            WriteFirstTypeLines(numberOfLine);
+            WriteSecondTypeLines(numberOfLine);
+        }
+
+        private static int getNumberOfLines()
+        {
+            return numberOfRepeat / 2;
+        }
+
+        private static void WriteSecondTypeLines(int numberOfRepeat)
+        {
+            for (int i = 0; i < numberOfRepeat / 2; i++)
+            {
+                WriteSecondTypeLine();
+            }
+        }
+
+        private static void WriteFirstTypeLines(int numberOfRepeat)
+        {
+            for (int i = 0; i < numberOfRepeat / 2; i++)
+            {
+                WriteFirstTypeLine();
+            }
+        }
+
+        private static void WriteFirstTypeLine()
+        {
+            WriteSymbol(sym1, numberOfRepeat);
+            WriteSymbol(sym2, numberOfRepeat);
+            WriteEndline();
+        }
+
+        private static void WriteSecondTypeLine()
+        {
+            WriteSymbol(sym2, numberOfRepeat);
+            WriteSymbol(sym1, numberOfRepeat);
+            WriteEndline();
+        }
+
+        static void WriteSymbol(Char symbol, int times)
+        {
+            for (int j = 0; j < times; j++)
+            {
+                Console.Write(symbol);
+            }
+        }
+
+        static void WriteEndline()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Zadanie2/Zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RadioButton checkedRadioButton = null;
+        private readonly List<RadioButton> radioButtons = new List<RadioButton>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            radioButtons.Add(Answer1);
+            radioButtons.Add(Answer2);
+            radioButtons.Add(Answer3);
+            radioButtons.Add(Answer4);
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            UncheckRadioButtons();
+            SetResultLabelContent();
+        }
+
+        private void Answer_Checked(object sender, RoutedEventArgs e)
+        {
+            checkedRadioButton = sender as RadioButton;
+
+            SetResultLabelContent();
+        }
+
+        private void SetResultLabelContent()
+        {
+            if (checkedRadioButton != null)
+            {
+                ResultField.Content = checkedRadioButton.Name;
+            } 
+            else
+            {
+                ResultField.Content = "";
+            }
+        }
+
+        private void UncheckRadioButtons()
+        {
+            if (checkedRadioButton != null)
+            {
+                checkedRadioButton.IsChecked = false;
+                checkedRadioButton = null;
+            }
+        }
+
+    }
+}
